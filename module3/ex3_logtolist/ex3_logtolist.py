ac56db2dc5e6314538e2382f26ae7f0e8e13351e
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-06 09:59:17 +0300

    ex3

--- module3/ex3_logtolist/ex3_logtolist.py
+++ module3/ex3_logtolist/ex3_logtolist.py
@@ -1,5 +1,11 @@
-def logtolist():
-    pass
+def logtolist(filename):
+    list_of_dict = []
+    with open(filename, 'r') as f:
+        for line in f:
+            my_dict = {'ip_address': line.split(' ', 1)[0], 'timestamp': line.split('[')[1].split(']')[0],
+                       'request': line.split('"')[1].split('"')[0]}
+            list_of_dict.append(my_dict)
+    return list_of_dict
 
 
 if __name__ == '__main__':
