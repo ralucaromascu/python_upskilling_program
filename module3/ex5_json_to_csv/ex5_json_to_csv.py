57a00036de71fa6c0b4ba63ab8dc67ec718f6027
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-06 09:59:40 +0300

    ex5

--- module3/ex5_json_to_csv/ex5_json_to_csv.py
+++ module3/ex5_json_to_csv/ex5_json_to_csv.py
@@ -1,9 +1,19 @@
+import csv
+import json
+from urllib.request import urlopen
+
 gist_url = 'https://gist.githubusercontent.com/Coraline13/d9cc3db0a77f548b3fe3a5f4267a9a9b/raw' \
            '/ba7ca3dff3acc140ca368b91bff195e2694383eb/cities.json'
 
 
-def cities_to_csv():
-    pass
+def cities_to_csv(my_url, filename):
+    response = urlopen(my_url)
+    data_json = json.loads(response.read())
+    csv_data = open(filename, 'w')
+    for info in data_json:
+        data = [info['city'], info['state'], info['rank'], info['population']]
+        wrt = csv.writer(csv_data, delimiter='\t')
+        wrt.writerow(data)
 
 
 if __name__ == '__main__':
