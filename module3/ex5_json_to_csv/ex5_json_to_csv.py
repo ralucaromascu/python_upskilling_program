9da0e8936534421bcd7a47c167b0896386843a3b
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-11 17:42:05 +0300

    ex5

--- module3/ex5_json_to_csv/ex5_json_to_csv.py
+++ module3/ex5_json_to_csv/ex5_json_to_csv.py
@@ -1,4 +1,3 @@
-import csv
 import json
 from urllib.request import urlopen
 
@@ -6,14 +5,19 @@ gist_url = 'https://gist.githubusercontent.com/Coraline13/d9cc3db0a77f548b3fe3a5
            '/ba7ca3dff3acc140ca368b91bff195e2694383eb/cities.json'
 
 
+def city_conv(info):
+    return info['city'] + "\t" + info['state'] + "\t" + info['rank'] + "\t" + info['population'] + "\n"
+
+
 def cities_to_csv(my_url, filename):
     response = urlopen(my_url)
-    data_json = json.loads(response.read())
+    data_json = json.loads(response.read(), object_hook=city_conv)
     csv_data = open(filename, 'w')
     for info in data_json:
-        data = [info['city'], info['state'], info['rank'], info['population']]
-        wrt = csv.writer(csv_data, delimiter='\t')
-        wrt.writerow(data)
+        # data = [info['city'], info['state'], info['rank'], info['population']]
+        # wrt = csv.writer(csv_data, delimiter='\t')
+        # wrt.writerow(data)
+        csv_data.write(info)
 
 
 if __name__ == '__main__':
