84f6f6f78bf31ab781a1e5070039adc02bec27fc
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-11 18:01:42 +0300

    ex8

--- module3/ex8_math_practice/ex8_math_practice.py
+++ module3/ex8_math_practice/ex8_math_practice.py
@@ -1,9 +1,30 @@
-def create_math_problems():
-    pass
+import random
+MIN = -40
+MAX = 40
+NR_LINES = 100
+WIDTH = max(len(str(MIN)), len(str(MAX)))+2
 
 
-def solve_math_problems():
-    pass
+def create_math_problems(tmp_path):
+    my_file = open(tmp_path, 'w')
+    index = 0
+
+    for one_line in range(NR_LINES):
+        index += 1
+        nr_1 = random.randint(MIN, MAX)
+        nr_2 = random.randint(-40, 40)
+        nr_3 = random.randint(-40, 40)
+        nr_4 = random.randint(-40, 40)
+        rand_sign = "+" if random.randint(0, 1) == 0 else "-"
+        line = "["+str(index).rjust(3)+"]"+str(nr_1).rjust(5)+" "+rand_sign+" " + "("+str(nr_2).rjust(4)+")" + " " + rand_sign+" " + "("+str(nr_3).rjust(4)+")" + " "+rand_sign+" " + "("+str(nr_4).rjust(4)+") = ______\n"
+        my_file.write(line)
+
+
+def solve_math_problems(tmp_path):
+    with open(tmp_path, 'r') as my_file:
+        for exercice in my_file:
+            result = eval(exercice.split(']', 1)[1].split('=', 1)[0])
+            print(f"{exercice.split('=', 1)[0]}= {result}")
 
 
 if __name__ == "__main__":
