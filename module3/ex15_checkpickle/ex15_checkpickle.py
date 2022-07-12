5c793ed744dd51635876f769597fd72276f52091
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-12 13:15:17 +0300

    ex15

--- module3/ex15_checkpickle/ex15_checkpickle.py
+++ module3/ex15_checkpickle/ex15_checkpickle.py
@@ -2,7 +2,7 @@ import os
 import pickle
 import time
 
-fields = ""
+fields = ['last_name', 'first_name', 'email']
 
 
 def get_people_from_checkpoint(cp_stem, timestamp):
@@ -10,14 +10,15 @@ def get_people_from_checkpoint(cp_stem, timestamp):
     timestamps_from_files = []
     people = []
     cp_stem = str(cp_stem)
-    obj = os.scandir(os.curdir)
+    obj = os.scandir(os.getcwd())
     with obj as it:
         for entry in it:
             if entry.is_file() and entry.name.startswith(cp_stem):
                 timestamps_from_files.append(float(entry.name.split(cp_stem)[1]))
+
     for timestamp_checkpoint in sorted(timestamps_from_files, reverse=True):
         if timestamp_checkpoint <= float(timestamp):
-            checkpoint = cp_stem + str(timestamp_checkpoint)
+            checkpoint = os.path.join(os.getcwd(), cp_stem + str(timestamp_checkpoint))
             break
 
     if checkpoint:
@@ -38,7 +39,7 @@ def add_person(cp_stem):
         people = get_people_from_checkpoint(cp_stem, time.time())
         people.append({'first_name': first_name, 'last_name': last_name, 'email': email})
         timestamp = str(time.time())
-        with open(cp_stem + timestamp, "wb") as f:
+        with open(os.path.join(os.getcwd(), cp_stem + timestamp), "wb") as f:
             pickle.dump(people, f)
 
 
@@ -55,7 +56,8 @@ def checkpickle(cp_stem='people-checkpoint-'):
 
         elif command == "l":
             people_list = get_people_from_checkpoint(cp_stem, float(time.time()))
-            print(people_list)
+            for person in people_list:
+                print(f"{person['last_name']}, {person['first_name']}: email {person['email']}\n")
 
         elif command == "a":
             add_person(cp_stem)
@@ -63,6 +65,7 @@ def checkpickle(cp_stem='people-checkpoint-'):
         elif command == "r":
             timestamp_want = input("Give me a timestamp: ")
             people_list = get_people_from_checkpoint(cp_stem, timestamp_want)
+            print(people_list)
         else:
             print("Sorry, wrong input.")
 
