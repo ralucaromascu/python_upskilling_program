92046cea317c5639f07416e7c250669f7f48a744
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-12 13:15:36 +0300

    update tests

--- module3/ex15_checkpickle/ex15_checkpickle_test.py
+++ module3/ex15_checkpickle/ex15_checkpickle_test.py
@@ -1,3 +1,4 @@
+import os
 from glob import glob
 from io import StringIO
 import pickle
@@ -13,28 +14,35 @@ add_two_people_and_list = StringIO('a\nfirst1\nlast1\n1111@\na\nfirst2\nlast2\n2
 
 def test_just_quit(monkeypatch, tmp_path):
     monkeypatch.setattr('sys.stdin', just_quit_input)
-    test_stem = tmp_path / 'test-cp'
+    test_stem = 'test-cp'
+    test_path = os.path.join(tmp_path, test_stem)
+    monkeypatch.setattr('os.getcwd', lambda: str(tmp_path))
     checkpickle(cp_stem=test_stem)
-    assert glob(f'{test_stem}*') == []
+    assert glob(f'{test_path}*') == []
 
 
 def test_add_one_person(monkeypatch, tmp_path):
     monkeypatch.setattr('sys.stdin', add_one_person_input)
-    test_stem = tmp_path / 'test-cp'
+    test_stem = 'test-cp'
+    test_path = os.path.join(tmp_path, test_stem)
+    monkeypatch.setattr('os.getcwd', lambda: str(tmp_path))
     checkpickle(cp_stem=test_stem)
-    assert len(glob(f'{test_stem}[0-9]*')) == 1
+    assert len(glob(f'{test_path}[0-9]*')) == 1
 
 
 def test_add_two_people(monkeypatch, tmp_path):
     monkeypatch.setattr('sys.stdin', add_two_people_input)
-    test_stem = tmp_path / 'test-cp'
+    test_stem = 'test-cp'
+    test_path = os.path.join(tmp_path, test_stem)
+    monkeypatch.setattr('os.getcwd', lambda: str(tmp_path))
     checkpickle(cp_stem=test_stem)
-    assert len(glob(f'{test_stem}*')) == 2
+    assert len(glob(f'{test_path}*')) == 2
 
 
 def test_add_two_people_and_list(capsys, monkeypatch, tmp_path):
     monkeypatch.setattr('sys.stdin', add_two_people_and_list)
-    test_stem = tmp_path / 'test-cp'
+    test_stem = 'test-cp'
+    monkeypatch.setattr('os.getcwd', lambda: str(tmp_path))
     checkpickle(cp_stem=test_stem)
     captured_out, captured_err = capsys.readouterr()
     outlines = captured_out.split('\n')
@@ -46,17 +54,20 @@ def test_add_two_people_and_list(capsys, monkeypatch, tmp_path):
 def test_glob_has_timestamp(monkeypatch, tmp_path):
     add_two_people_and_list.seek(0)
     monkeypatch.setattr('sys.stdin', add_two_people_and_list)
-    test_stem = tmp_path / 'test-cp'
+    test_stem = 'test-cp'
+    monkeypatch.setattr('os.getcwd', lambda: str(tmp_path))
     checkpickle(cp_stem=test_stem)
-    assert len(glob(f'{test_stem}*{int(time.time())}*')) == 2
+    assert len(glob(f'{os.path.join(tmp_path, test_stem)}*{int(time.time())}*')) == 2
 
 
 def test_file_contains_pickled_list_of_dicts(monkeypatch, tmp_path):
     add_two_people_and_list.seek(0)
     monkeypatch.setattr('sys.stdin', add_two_people_and_list)
-    test_stem = tmp_path / 'test-cp'
+    test_stem = 'test-cp'
+    test_path = os.path.join(tmp_path, test_stem)
+    monkeypatch.setattr('os.getcwd', lambda: str(tmp_path))
     checkpickle(cp_stem=test_stem)
-    filename = glob(f'{test_stem}*{int(time.time())}*')[0]
+    filename = glob(f'{test_path}*{int(time.time())}*')[0]
     people = pickle.load(open(filename, 'rb'))
     assert type(people) == list
     assert all([type(one_person) == dict
