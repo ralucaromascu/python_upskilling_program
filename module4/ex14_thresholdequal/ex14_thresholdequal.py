9018559ebef427b5e93d16badf04116ea475f120
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-15 11:00:02 +0300

    ex14

--- module4/ex14_thresholdequal/ex14_thresholdequal.py
+++ module4/ex14_thresholdequal/ex14_thresholdequal.py
@@ -1,5 +1,29 @@
 class ThresholdEqual:
-    pass
+    def __init__(self, x):
+        self.x = x
+        self.tolerance = 2
+
+    def __eq__(self, other):
+        return abs(self.x - other.x) <= self.tolerance
+
+    def __add__(self, other):
+        return self.x + other.x
+
+    def __sub__(self, other):
+        return self.x - other.x
+
+    def __mul__(self, other):
+        return self.x * other.x
+
+    def __truediv__(self, other):
+        # test cu impartirea la 0
+        return self.x / other.x
+
+    def __mod__(self, other):
+        return self.x % self.x
+
+    def __pow__(self, other, modulo=None):
+        return pow(self.x, other.x)
 
 
 if __name__ == '__main__':
