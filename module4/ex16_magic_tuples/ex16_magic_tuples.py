6ee81d14a669168f2641eef1ae68e7e8ee1ffe6d
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-18 13:14:48 +0300

    fix ex16

--- module4/ex16_magic_tuples/ex16_magic_tuples.py
+++ module4/ex16_magic_tuples/ex16_magic_tuples.py
@@ -1,6 +1,7 @@
 def magic_tuples(total, max_value):
     for i in range(total - max_value + 1, max_value):
-        yield i, total - i
+        if i > 0 and total - i > 0:
+            yield i, total - i
 
 
 if __name__ == '__main__':
