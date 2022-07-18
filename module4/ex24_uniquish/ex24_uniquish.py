ba35297259e42da639aa72e6bc056ba4179d530c
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-18 09:04:27 +0300

    ex24

--- module4/ex24_uniquish/ex24_uniquish.py
+++ module4/ex24_uniquish/ex24_uniquish.py
@@ -2,15 +2,34 @@ class Foo:
     def __init__(self, x):
         self.x = x
 
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.x == other.x
+        return False
+
+    def __hash__(self):
+        return hash(self.x)
+
 
 class Uniquish:
-    pass
+    def __eq__(self, other):
+        if type(self) == type(other):
+            return self.__dict__ == other.__dict__
+
+    def __hash__(self):
+         return hash(tuple(sorted(self.__dict__.items())))
 
 
 class Bar(Uniquish):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        return super(Bar, self).__eq__(other)
+
+    def __hash__(self):
+        return super(Bar, self).__hash__()
 
 
 if __name__ == '__main__':
@@ -23,5 +42,7 @@ if __name__ == '__main__':
     b1 = Bar(10, 'abc')
     b2 = Bar(10, 'abc')
     b3 = Bar(10, 'abc')
+    print(hash(b1))
+    print(hash(b2))
     s = {b1, b2, b3}
     print(s)
