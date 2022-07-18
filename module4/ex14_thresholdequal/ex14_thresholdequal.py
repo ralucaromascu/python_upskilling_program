87c864084756f5f1f8af908f1e2694c69b47fef5
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-18 17:27:25 +0300

    update ex14

--- module4/ex14_thresholdequal/ex14_thresholdequal.py
+++ module4/ex14_thresholdequal/ex14_thresholdequal.py
@@ -6,24 +6,33 @@ class ThresholdEqual:
     def __eq__(self, other):
         return abs(self.x - other.x) <= self.tolerance
 
+    def __lt__(self, other):
+        return self.x - other.x < 0 and abs(self.x - other.x) > self.tolerance
+
+    def __gt__(self, other):
+        return self.x - other.x > 0 and abs(self.x - other.x) > self.tolerance
+
     def __add__(self, other):
-        return self.x + other.x
+        return ThresholdEqual(self.x + other.x)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        return self
 
     def __sub__(self, other):
-        return self.x - other.x
+        return ThresholdEqual(self.x - other.x)
 
     def __mul__(self, other):
-        return self.x * other.x
+        return ThresholdEqual(self.x * other.x)
 
     def __truediv__(self, other):
-        # test cu impartirea la 0
-        return self.x / other.x
+        return ThresholdEqual(self.x / other.x)
 
     def __mod__(self, other):
-        return self.x % self.x
+        return ThresholdEqual(self.x % self.x)
 
     def __pow__(self, other, modulo=None):
-        return pow(self.x, other.x)
+        return ThresholdEqual(pow(self.x, other.x))
 
 
 if __name__ == '__main__':
@@ -34,3 +43,6 @@ if __name__ == '__main__':
     te1 = ThresholdEqual(10)
     te2 = ThresholdEqual(9)
     print(te1 == te2)
+    print(type(te1+te2))
+    te2 += te1
+    print(te2.x)
