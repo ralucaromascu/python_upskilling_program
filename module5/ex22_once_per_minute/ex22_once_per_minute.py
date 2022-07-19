0749e53ce2698a9d254d2ce3ae8b224373b3b7fa
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-19 17:46:23 +0300

    ex22

--- module5/ex22_once_per_minute/ex22_once_per_minute.py
+++ module5/ex22_once_per_minute/ex22_once_per_minute.py
@@ -1,3 +1,31 @@
+import time
+start_time = 0
+is_first_time = True
+
+
+class TooSoonError(Exception):
+    pass
+
+
+def once_per_minute(func):
+    def wrapper(*args, **kwargs):
+        global start_time
+        global is_first_time
+        actual_time = time.perf_counter()
+        if is_first_time is True:
+            start_time = actual_time
+            is_first_time = False
+        print(actual_time)
+        print(start_time)
+        time_diff = actual_time-start_time
+        if int(time_diff) % 60 != 0:
+            raise TooSoonError(f'Too soon: Wait another {60 - time_diff % 60} seconds')
+        else:
+            func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return wrapper
+
+
 if __name__ == '__main__':
     @once_per_minute
     def hello(name):
