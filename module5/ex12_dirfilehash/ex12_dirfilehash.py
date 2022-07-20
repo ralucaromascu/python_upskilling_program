36fec7c753c65070cdbcdf2c2cc820ba045aae77
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-20 19:08:48 +0300

    ex12

--- module5/ex12_dirfilehash/ex12_dirfilehash.py
+++ module5/ex12_dirfilehash/ex12_dirfilehash.py
@@ -1,6 +1,25 @@
+import hashlib
+import os
+
+
 class DirFileHash:
-    # test
-    pass
+    def __init__(self, dir_name):
+        self.dirname = dir_name
+        self.files = {}
+        if os.path.isdir(dir_name):
+            for entry in os.scandir(dir_name):
+                if entry.is_file():
+                    try:
+                        with open(entry, 'rb') as f:
+                            read_content = f.read()
+                        hash_value = hashlib.md5(read_content).hexdigest()
+                        self.files[entry] = hash_value
+                    except IOError as e:
+                        print(e)
+
+    def __getitem__(self, item):
+        if item in self.files:
+            return self.files[item]
 
 
 if __name__ == '__main__':
