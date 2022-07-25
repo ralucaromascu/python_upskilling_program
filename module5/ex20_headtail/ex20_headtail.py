2816967a10305c8f2bbd3c0850c0aca23f7c5b88
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-25 17:24:29 +0300

    solve ex20

--- module5/ex20_headtail/ex20_headtail.py
+++ module5/ex20_headtail/ex20_headtail.py
@@ -1,17 +1,24 @@
 import argparse
+import os
 
 
 def head(given_file, nr_lines):
-    print(f'first {nr_lines} from {given_file}')
+    print(f'First {nr_lines} from {os.path.basename(given_file)}:')
+    with open(given_file, 'r') as f:
+        lines = f.readlines()
+    print(*lines[:nr_lines], sep='')
 
 
 def tail(given_file, nr_lines):
-    print(f'last {nr_lines} from {given_file}')
+    print(f'Last {nr_lines} from {os.path.basename(given_file)}:')
+    with open(given_file, 'r') as f:
+        lines = f.readlines()
+    print(*lines[-nr_lines:], sep='')
 
 
 def headtail():
     my_parser = argparse.ArgumentParser()
-    my_parser.add_argument('-f', '--file', help="file to read", nargs=1, required=True)
+    my_parser.add_argument('-f', '--file', help="file to read", required=True, type=str)
 
     my_parser.add_argument('-s', '--start', help="number of lines to read from head of the file", nargs='?', const=3,
                            type=int)
@@ -19,6 +26,7 @@ def headtail():
                            type=int)
 
     args = my_parser.parse_args()
+    print(args)
     if args.start:
         head(args.file, args.start)
     if args.end:
