2b7e18bf70ce069853162b2986b097a70a0bc251
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-25 16:33:36 +0300

    update ex20

--- module5/ex20_headtail/ex20_headtail.py
+++ module5/ex20_headtail/ex20_headtail.py
@@ -1,35 +1,28 @@
 import argparse
-my_file = None
 
 
-class Head(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        if values is None:
-            values = 3
-        print(f'Here I am, setting the values {values} for the {option_string} option.. from file {my_file}.')
-        setattr(namespace, self.dest, values)
-        print(namespace)
+def head(given_file, nr_lines):
+    print(f'first {nr_lines} from {given_file}')
 
 
-class Tail(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        if values is None:
-            values = 3
-        print('Here I am, setting the values %r for the %r option...' % (values, option_string))
-        setattr(namespace, self.dest, values)
+def tail(given_file, nr_lines):
+    print(f'last {nr_lines} from {given_file}')
 
 
 def headtail():
     my_parser = argparse.ArgumentParser()
-    my_parser.add_argument('-f', '--file', help="file to read", dest="file")
+    my_parser.add_argument('-f', '--file', help="file to read", nargs=1, required=True)
 
-    my_parser.add_argument('-s', '--start', help="number of lines to read from head of the file", action=Head, nargs='?')
-    my_parser.add_argument('-e', '--end', help="number of lines to read from head of the file", action=Tail, nargs='?')
+    my_parser.add_argument('-s', '--start', help="number of lines to read from head of the file", nargs='?', const=3,
+                           type=int)
+    my_parser.add_argument('-e', '--end', help="number of lines to read from head of the file", nargs='?', const=3,
+                           type=int)
 
     args = my_parser.parse_args()
-    global my_file
-    my_file = args.file
-    print(my_file)
+    if args.start:
+        head(args.file, args.start)
+    if args.end:
+        tail(args.file, args.end)
 
 
 if __name__ == '__main__':
