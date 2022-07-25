f5994dc21318d9ce6489d9af6b4a324396af0c37
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-25 11:27:28 +0300

    start ex25

--- module6/ex25_file_info/ex25_file_info.py
+++ module6/ex25_file_info/ex25_file_info.py
@@ -3,6 +3,12 @@ import os
 def get_file_info():
     pass
 
+def get_file_info(pathname):
+    for (root, dirs, files) in os.walk(pathname, topdown=True):
+        print(root)
+        print(dirs)
+        print(files)
+
 
 if __name__ == '__main__':
-    pass
+        get_file_info(os.getcwd())
