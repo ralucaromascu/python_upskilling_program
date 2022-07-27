ec4e6ebb5a9ddc6b19e1b9646102614cb51b3519
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-27 16:20:00 +0300

    update ex38

--- module5/ex38_tar_to_zip/ex38_tar_to_zip.py
+++ module5/ex38_tar_to_zip/ex38_tar_to_zip.py
@@ -3,10 +3,6 @@ import tarfile
 import zipfile
 
 
-class NotTarFile(Exception):
-    pass
-
-
 def create_tarfile(tarfile_name):
     for index, letter in enumerate('abcde', 1):
         with open(os.path.join(os.getcwd(), f'{letter * index}.txt'), 'w') as f:
@@ -24,27 +20,26 @@ def tar_to_zip(*tar_files, zippath=None):
     if zippath is None:
         zippath = os.getcwd()
     for tar_file in tar_files:
-        if not os.path.exists(os.path.join(os.getcwd(), tar_file)):
-            raise FileNotFoundError
-        if not tarfile.is_tarfile(tar_file):
-            raise NotTarFile(f"{tar_file} is not a tar file.")
-        prefix = str(tar_file).rsplit('.', 1)[0]
-        print(prefix)
-        print(tar_file)
-        tar = tarfile.open(tar_file)
-        tar.extractall(prefix)
-        tar.close()
-
-        with zipfile.ZipFile(f'{os.path.join(zippath, prefix)}.zip', 'w') as zipObj:
-            for folderName, subfolders, filenames in os.walk(prefix):
-                for filename in filenames:
-                    file_path = os.path.join(folderName, filename)
-                    zipObj.write(file_path)
+        try:
+            tar = tarfile.open(tar_file)
+            prefix = str(tar_file).rsplit('.', 1)[0]
+            tar.extractall(prefix)
+            print(prefix)
+            tar.close()
+            with zipfile.ZipFile(f'{os.path.join(zippath, prefix)}.zip', 'w') as zipObj:
+                for folderName, subfolders, filenames in os.walk(prefix):
+                    for filename in filenames:
+                        file_path = os.path.join(folderName, filename)
+                        zipObj.write(file_path)
+        except FileNotFoundError:
+            print(f"Error: {tar_file} not found")
+        except IsADirectoryError:
+            print(f"Error: {tar_file} is not a tar file.")
 
 
 if __name__ == '__main__':
     # create_tarfile('foo.tar')
     # create_tarfile('bar.tar.gz')
     # create_tarfile('baz.tar.bz2')
-    tar_to_zip('foo.tar', 'bar.tar.gz', 'baz.tar.bz2', 'test.txt',
+    tar_to_zip('foo.tar', 'bar.tar.gz', 'test.txt', 'blabla', 'baz.tar.bz2',
                zippath="/Users/raluca.romascu/python_upskilling_program/module5/ex38_tar_to_zip/test_zippath")
