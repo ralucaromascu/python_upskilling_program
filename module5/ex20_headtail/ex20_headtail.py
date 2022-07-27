fb087c1c5d94fde79872b72c58ec5c90b72f61a5
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-07-27 15:43:20 +0300

    update ex20

--- module5/ex20_headtail/ex20_headtail.py
+++ module5/ex20_headtail/ex20_headtail.py
@@ -2,24 +2,31 @@ import argparse
 import os
 
 
+def positive_int(nr):
+    try:
+        integer = int(nr)
+    except:
+        raise argparse.ArgumentTypeError('values for arguments -s/--start and -e/--end should be integer numbers')
+    if integer <= 0:
+        raise argparse.ArgumentTypeError('values for arguments -s/--start and -e/--end should be positive numbers')
+    return integer
+
+
 def headtail():
     my_parser = argparse.ArgumentParser()
-    my_parser.add_argument('-f', '--file', help="file to read", required=True, type=str)
+    my_parser.add_argument('-f', '--file', help="file to read", required=True, type=argparse.FileType('r'))
 
     my_parser.add_argument('-s', '--start', help="number of lines to read from head of the file", default=3,
-                           type=int)
+                           type=positive_int)
     my_parser.add_argument('-e', '--end', help="number of lines to read from head of the file", default=3,
-                           type=int)
-
+                           type=positive_int)
     args = my_parser.parse_args()
     print(args)
-    if args.start < 0 or args.end < 0:
-        raise ValueError("values for arguments -s/--start and -e/--end should be positive numbers")
-    with open(args.file, 'r') as f:
+    with open(args.file.name, args.file.mode) as f:
         lines = f.readlines()
-    print(f'First {args.start} from {os.path.basename(args.file)}:')
+    print(f'First {args.start} from {os.path.basename(args.file.name)}:')
     print(*lines[:args.start], sep='')
-    print(f'Last {args.end} from {os.path.basename(args.file)}:')
+    print(f'Last {args.end} from {os.path.basename(args.file.name)}:')
     print(*lines[-args.end:], sep='')
 
 
