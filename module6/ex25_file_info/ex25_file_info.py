207b5714fa8b408f3e468573d8911505d556a630
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-08-01 12:50:29 +0300

    update ex25

--- module6/ex25_file_info/ex25_file_info.py
+++ module6/ex25_file_info/ex25_file_info.py
@@ -1,6 +1,7 @@
 import hashlib
 import os
 from datetime import datetime
+from pathlib import Path
 
 
 def get_sha1(pathfile):
@@ -12,29 +13,36 @@ def get_sha1(pathfile):
             if not chunk:
                 break
             md.update(chunk)
-        print(md.hexdigest())
         file_hashed_sha1 = md.hexdigest()
     return file_hashed_sha1
 
 
 def get_file_info(pathname):
+    abspath = os.path.abspath(pathname)
 
-    files_dict = []
-    for (root, dirs, files) in os.walk(pathname, topdown=True):
-        for file in files:
-            pathfile = os.path.join(root, file)
-            file_info = os.stat(pathfile)
-            atime = datetime.fromtimestamp(file_info.st_atime).strftime('%Y-%m-%d %H:%M')
-            mtime = datetime.fromtimestamp(file_info.st_mtime).strftime('%Y-%m-%d %H:%M')
-            ctime = datetime.fromtimestamp(file_info.st_ctime).strftime('%Y-%m-%d %H:%M')
-            files_dict.append({'filename': file,
-                               'access_time': atime,
-                               'modification_time': mtime,
-                               'creation_time': ctime,
-                               'sha1': get_sha1(pathfile)})
-    return files_dict
+    try:
+        if not Path(abspath).is_dir():
+            raise NotADirectoryError
+        files_dict = []
+        for (root, dirs, files) in os.walk(abspath, topdown=True):
+            for file in files:
+                pathfile = os.path.join(root, file)
+                file_info = os.stat(pathfile)
+                atime = datetime.fromtimestamp(file_info.st_atime).strftime('%Y-%m-%d %H:%M')
+                mtime = datetime.fromtimestamp(file_info.st_mtime).strftime('%Y-%m-%d %H:%M')
+                ctime = datetime.fromtimestamp(file_info.st_ctime).strftime('%Y-%m-%d %H:%M')
+                files_dict.append({'pathfile': pathfile,
+                                   'filename': file,
+                                   'access_time': atime,
+                                   'modification_time': mtime,
+                                   'creation_time': ctime,
+                                   'sha1': get_sha1(pathfile)})
+        return files_dict
+    except NotADirectoryError:
+        return f"Error: The directory given: [{pathname}] is not a valid one"
 
 
 if __name__ == '__main__':
-
+    print(get_file_info("Fvds"))
+    print(get_file_info("../ex25_file_info"))
     print(get_file_info(os.getcwd()))
