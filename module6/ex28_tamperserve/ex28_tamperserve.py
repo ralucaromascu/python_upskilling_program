85d52d6c8fd4cc92cd0ad20010f8d3bdfa903cda
ralucaromascu <108052842+ralucaromascu@users.noreply.github.com>
2022-08-01 15:47:49 +0300

    update ex28

--- module6/ex28_tamperserve/ex28_tamperserve.py
+++ module6/ex28_tamperserve/ex28_tamperserve.py
@@ -38,6 +38,39 @@ class FileList:
         self.all_file_infos = []
         self.last_timestamp = 0
 
+    def scan(self):
+        for (root, dirs, files) in os.walk(self.dir_path, topdown=True):
+            for file in files:
+                pathfile = os.path.join(root, file)
+                file_info = os.stat(pathfile)
+                self.all_file_infos.append(FileInfo(file, file_info.st_mtime, get_sha1(pathfile)))
+        self.last_timestamp = time.time()
+
+    def rescan(self):
+        update_dict = {'added': [], 'removed': [], 'changed': [], 'unchanged': []}
+        new_files_list = []
+        for (root, dirs, files) in os.walk(self.dir_path, topdown=True):
+            for file in files:
+                pathfile = os.path.join(root, file)
+                file_info = os.stat(pathfile)
+                new_files_list.append(FileInfo(file, file_info.st_mtime, get_sha1(pathfile)))
+        for file_obj in new_files_list:
+            if file_obj not in self.all_file_infos:
+                update_dict['added'].append(getattr(file_obj, 'filename'))
+        for file_obj in self.all_file_infos:
+            if file_obj not in new_files_list:
+                update_dict['removed'].append(getattr(file_obj, 'filename'))
+        for file_obj1 in new_files_list:
+            for file_obj2 in self.all_file_infos:
+                if file_obj1 == file_obj2 :
+                    if getattr(file_obj1, 'sha1') != getattr(file_obj2, 'sha1'):
+                        update_dict['changed'].append(getattr(file_obj1, 'filename'))
+                    else:
+                        update_dict['unchanged'].append(getattr(file_obj1, 'filename'))
+        self.all_file_infos = new_files_list
+        self.last_timestamp = time.time()
+        return update_dict
+
 
 app = Flask(__name__)
 
@@ -49,13 +82,8 @@ def scan():
         return f'The given directory is not a valid one.'
     else:
         fl = FileList(dir_path)
-        for (root, dirs, files) in os.walk(dir_path, topdown=True):
-            for file in files:
-                pathfile = os.path.join(root, file)
-                file_info = os.stat(pathfile)
-                fl.all_file_infos.append(FileInfo(file, file_info.st_mtime, get_sha1(pathfile)))
-        fl.last_timestamp = time.time()
-        with open(dir_path.replace('/', '-'), "wb") as f:
+        fl.scan()
+        with open(os.path.join(dir_path, dir_path.replace('/', '-')), "wb") as f:
             pickle.dump(fl, f)
         return "scan"
 
@@ -69,33 +97,15 @@ def rescan():
         is_scanned = False
         pickle_name_dir = dir_path.replace('/', '-')
         new_fl = FileList(dir_path)
-        for (root, dirs, files) in os.walk(dir_path, topdown=True):
-            for file in files:
-                pathfile = os.path.join(root, file)
-                file_info = os.stat(pathfile)
-                if file == pickle_name_dir:
-                    is_scanned = True
-                else:
-                    new_fl.all_file_infos.append(FileInfo(file, file_info.st_mtime, get_sha1(pathfile)))
-        new_fl.last_timestamp = time.time()
+        new_fl.scan()
+        if pickle_name_dir in [file_obj.filename for file_obj in new_fl.all_file_infos]:
+            is_scanned = True
         if not is_scanned:
             return f'The given directory was not scanned before.'
         else:
-            with open(pickle_name_dir, "rb") as f:
+            with open(os.path.join(dir_path, pickle_name_dir), "rb") as f:
                 fl = pickle.load(f)
-
-            update_dict = {'added': [], 'removed': [], 'changed': []}
-
-            for file_obj in new_fl.all_file_infos:
-                if file_obj not in fl.all_file_infos:
-                    update_dict['added'].append(getattr(file_obj, 'filename'))
-            for file_obj in fl.all_file_infos:
-                if file_obj not in new_fl.all_file_infos:
-                    update_dict['removed'].append(getattr(file_obj, 'filename'))
-            for file_obj1 in new_fl.all_file_infos:
-                for file_obj2 in fl.all_file_infos:
-                    if file_obj1 == file_obj2 and getattr(file_obj1, 'sha1') != getattr(file_obj2, 'sha1'):
-                        update_dict['changed'].append(getattr(file_obj1, 'filename'))
-            with open(dir_path.replace('/', '-'), "wb") as f:
+            update_dict = fl.rescan()
+            with open(os.path.join(dir_path, dir_path.replace('/', '-')), "wb") as f:
                 pickle.dump(new_fl, f)
             return update_dict
